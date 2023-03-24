e224f597af5f23fbf44185516b5b00a342334f69
harper wen <owilalala974@gmial.com>
2023-03-24 09:54:56 +0800

    chroe: add base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/HAMAYA-vite-2.0/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
