d0860290c9e88d2b65e2a32a45ac16cf0138a51e
harper wen <owilalala974@gmial.com>
2023-04-17 17:13:38 +0800

    chore: fix path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteSvgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSvgr()],
   base: '/HAMAYA-vite-2.0',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-      // '@/component': path.resolve(__dirname, './src/components'),
-      // '@/styles': path.resolve(__dirname, './src/styles'),
-      // '@/stories_modules': path.resolve(__dirname, './src/stories/modules')
+      '@': path.resolve(__dirname, './src'),
+      '@Component': path.resolve(__dirname, './src/components'),
+      '@Styles': path.resolve(__dirname, './src/styles'),
+      '@Stories_modules': path.resolve(__dirname, './src/stories/modules'),
+      // '@Stories_components': path.resolve(__dirname, './src/stories/components'),
+      '@Images': path.resolve(__dirname, './public/images')
     }
   }
 });
