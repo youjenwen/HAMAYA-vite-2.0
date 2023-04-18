47973c4d2cbb0f99120d3dad6b90930da327b365
harper wen <owilalala974@gmial.com>
2023-04-18 14:54:01 +0800

    feat: optimize component

--- src/stories/modules/pageHeader/index.tsx
+++ src/stories/modules/pageHeader/index.tsx
@@ -3,20 +3,46 @@ import { Layout, Image } from 'antd';
 const { Header: HeaderAntd } = Layout;
 import Menu from '@Stories_modules/menu';
 import Button from '@Stories_modules/button';
-import Logo from '@Images/logo-horizontal.svg';
 import SvgIcon from '@Stories_modules/svgIcon';
+import Logo from '@Images/logo-white.svg';
 import './pageHeader.scss';
-import { DownloadOutlined } from '@ant-design/icons';
 
 const PageHeader = () => {
   return (
     <>
       <HeaderAntd className="flex items-center justify-center gap-3 bg-primary-garyLight text-white">
-        {/* <Image className="" preview={false} src={Logo} /> */}
+        <Image className="" preview={false} src={Logo} />
         <Menu />
-        <Button className='bg-primary-garyLight' icon={<SvgIcon wrapperStyle="flex items-center justify-center" iconName="users" svgProp={{ width: 25, height: 25 }} />} />
-        <Button className='bg-primary-garyLight' icon={<SvgIcon wrapperStyle="flex items-center justify-center" iconName="shopping_cart" svgProp={{ width: 25, height: 25 }} />} />
-        <Button className='bg-primary-garyLight' icon={<SvgIcon wrapperStyle="flex items-center justify-center" iconName="logout" svgProp={{ width: 25, height: 25 }} />} />
+        <Button
+          className="bg-primary-garyLight"
+          icon={
+            <SvgIcon
+              wrapperStyle="flex items-center justify-center"
+              iconName="users"
+              svgProp={{ width: 25, height: 25 }}
+            />
+          }
+        />
+        <Button
+          className="bg-primary-garyLight"
+          icon={
+            <SvgIcon
+              wrapperStyle="flex items-center justify-center"
+              iconName="shopping_cart"
+              svgProp={{ width: 25, height: 25 }}
+            />
+          }
+        />
+        <Button
+          className="bg-primary-garyLight"
+          icon={
+            <SvgIcon
+              wrapperStyle="flex items-center justify-center"
+              iconName="logout"
+              svgProp={{ width: 25, height: 25 }}
+            />
+          }
+        />
       </HeaderAntd>
     </>
   );
