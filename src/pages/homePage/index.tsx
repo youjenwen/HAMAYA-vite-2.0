865e34c3344f8f4c9867b42feb6a902e0e043456
harper wen <owilalala974@gmial.com>
2023-06-14 15:07:07 +0800

    feat: add service section in homePage

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,3 +1,4 @@
+import HomeService from '@Components/homeService';
 import VideoBanner from '@Stories_components/videoBanner';
 import React from 'react';
 
@@ -5,6 +6,8 @@ const HomePage = () => {
   return (
     <div>
       <VideoBanner />
+      {/* TODO: 有一個 幻燈箱 */}
+      <HomeService />
     </div>
   );
 };
